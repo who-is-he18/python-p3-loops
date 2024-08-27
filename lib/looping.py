cb4b5ef2681f771545f965f84727acaf52d704ec
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-08-27 12:50:47 +0300

    lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
-#!/usr/bin/env python3
+
+#!/usr/bin/env python3#-
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    return [num ** 2 for num in int_list]#-
 def fizzbuzz():
-    # code goes here!
-    pass
+    # code goes here!#-
+#-
+    pass#-
+    for num in range(1, 101):#+
+        if num % 3 == 0 and num % 5 == 0:#+
+            print("FizzBuzz")#+
+        elif num % 3 == 0:#+
+            print("Fizz")#+
+        elif num % 5 == 0:#+
+            print("Buzz")#+
+        else:#+
+            print(num)#+
+
